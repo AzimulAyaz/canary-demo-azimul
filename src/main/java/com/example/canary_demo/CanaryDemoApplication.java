7d6d4a15113a99f98f14808ee571158b46cde226
azimul.mynt@perennialsys.com <azimul.mynt@perennialsys.com>
2026-01-28 09:21:27 +0530

    feat: Initial setup done and login endpoint added for v1

---
+++ src/main/java/com/example/canary_demo/CanaryDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.canary_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanaryDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanaryDemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/canary_demo/controller/AuthController.java
@@ -0,0 +1,19 @@
+package com.example.canary_demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1")
+public class AuthController {
+
+    @GetMapping("/login")
+    public ResponseEntity<String> login(){
+        String loginInfo = "User Login Successful!!";
+        return ResponseEntity.ok(loginInfo);
+    }
+
+
+}
